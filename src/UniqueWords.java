614c5c573724609e16bd321d8c0a669ea4ffa697
Prof. G <ghazizap@stjohns.edu>
2021-02-20 13:29:13 -0500

    first version of the project

---
+++ src/UniqueWords.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class UniqueWords
+{
+   /**
+		counts the number of unique strings in a list
+		@param list ArrayList of strings to be examined
+		@return number of unique strings in the list
+   */
+   public static int countUnique(ArrayList<String> list)
+   {
+	  int count = 0;
+	  
+      for (int i = 0; i < list.size(); i++)
+      {		 for (int j = 0; j < list.size(); j++)
+		 {
+			
+		 }
+      }
+	  return count;
+   }
+
+   public static void main(String[] args)
+   {
+      ArrayList <String> words = new ArrayList<>();
+      words.add("apple");
+	  words.add("orange");
+	  words.add("blackboard");
+	  words.add("apple");
+	  words.add("orange");
+	  words.add("sun");
+	  words.add("moon");
+   
+	  int unique = countUnique(words);
+      System.out.println(words + " has " + unique + " unique words");
+   }
+}
