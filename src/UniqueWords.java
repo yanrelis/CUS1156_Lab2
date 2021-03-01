15922ca61bcee497b6c3dc7ad6af24607ac55398
Yanelis Cruceta <yanelis@yanelis-mbp.home>
2021-02-28 21:56:12 -0500

    commit

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,38 @@
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
+public class UniqueWords {
+	/**
+	 * counts the number of unique strings in a list
+	 * 
+	 * @param list ArrayList of strings to be examined
+	 * @return number of unique strings in the list
+	 */
+	public static int countUnique(ArrayList<String> list) {
+		int count = 0;
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size(); j++) {
+				if (list.get(i) == list.get(j)) {
+					count--;
+				} else if (list.get(i) != list.get(j)) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+
+		int unique = countUnique(words);
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
